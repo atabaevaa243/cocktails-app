f865452391ad055dfd0d56243df5c4bc81d55e71
Aziza <atabaevaa243@gmail.com>
2023-03-15 00:39:37 +0600

    Initial Commit

---
+++ Homework2_M5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2_M5
+//
+//  Created by Aziza A on 15/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
